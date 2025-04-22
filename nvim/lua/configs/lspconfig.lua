7cd8b4cba692898731eab88c6a8299f876b8f73f
Tim Seagren <timseagren@defenseunicorns.com>
2025-04-22 15:51:10 -0700

    a few tweaks

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -3,7 +3,6 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
--- EXAMPLE
 local servers = { "html", "cssls" , "gopls", "bashls", "yamlls", "terraformls"}
 local nvlsp = require "nvchad.configs.lspconfig"
 
