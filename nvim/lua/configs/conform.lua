38cd585e2ea83765f787a2907382d165880b94fe
Tim Seagren <timseagren@defenseunicorns.com>
2025-04-23 11:32:12 -0700

    update to add mason and setup tofu formatter

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -3,14 +3,14 @@ local options = {
     lua = { "stylua" },
     css = { "prettier" },
     html = { "prettier" },
-    terraform = { "terraform-ls" }
+    terraform = { "tofu_fmt" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -3,12 +3,29 @@ return {
     "stevearc/conform.nvim",
     event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
+    lazy = false
   },
 
   {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
+    end,
+  },
+
+   {
+    "williamboman/mason.nvim",
+    cmd = { "Mason", "MasonInstall", "MasonUpdate", "MasonInstallAll" },
+    opts = function()
+      return require "nvchad.configs.mason"
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    event = "User FilePost",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
     end,
   },
 
