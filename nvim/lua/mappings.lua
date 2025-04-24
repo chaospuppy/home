f26cebb3d160a4794013e117c433ec9a9566930d
Tim Seagren <timseagren@defenseunicorns.com>
2025-04-24 15:43:10 -0700

    add mapping for error showing, add check for autosourcing kitchen

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -6,5 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<leader>gl", "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
